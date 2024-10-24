5bdd86eff2c816eea90ef1076df90cec84afeea5
JeppeBC <202106888@post.au.dk>
2024-10-24 10:21:14 +0200

    Refactor queue.h for consistency and add stack functionality

--- queue.h
+++ queue.h
@@ -10,8 +10,16 @@ typedef struct queue {
   node *rear;
 } queue;
 
+typedef struct {
+  node *head;
+} stack;
+
 void initialize(queue *q);
 bool empty(const queue *q);
 bool full(const queue *q);
 void enqueue(queue *q, int x);
 int dequeue(queue *q);
+void push(int element, node **head);
+int pop(node **head);
+void enqueueStack(queue *q, int x);
+int dequeueStack(queue *q);
