c57da3db36a695dc2e6b5c899cd7f6fbc8bf49cd
SimonHvidbergNygaard <202409333@post.au.dk>
2024-10-24 12:51:46 +0200

    Mainawd awd

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,6 +1,7 @@
 #include "insertion_sort.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 node* isort(node *list) {
 
 }
-
--- queue.c
+++ queue.c
@@ -1,21 +1,21 @@
-#include "queue.h"
+// #include "queue.h"
 
-void initialize(queue *q) {
+// void initialize(queue *q) {
 
-}
+// }
 
-bool empty(const queue *q) {
+// bool empty(const queue *q) {
 
-}
+// }
 
-bool full(const queue *q) {
+// bool full(const queue *q) {
 
-}
+// }
 
-void enqueue(queue *q, int x) {
+// void enqueue(queue *q, int x) {
 
-}
+// }
 
-int dequeue(queue *q) {
+// int dequeue(queue *q) {
 
-}
+// }
